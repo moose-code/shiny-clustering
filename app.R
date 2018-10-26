f769acf5ba1d2cff69afe37313b5ce055f25e079
jonjon <jonjonclark@gmail.com>
2018-10-26 08:48:48 +0200

    added world map

--- app.R
+++ app.R
@@ -1,6 +1,8 @@
 library(shiny)
 library(shinythemes)
 library(factoextra) 
+library(rworldmap)
+library(rworldxtra)
 
 
 require(gdata)
@@ -28,8 +30,11 @@ library(gridExtra)
 plot1 <- fviz_contrib(pca.out, choice="var", axes = 1, top = 19)
 plot2 <- fviz_contrib(pca.out, choice="var", axes = 2, top = 19, color = "lightgrey")
 
+#my_palette <- palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3","#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
+#my_palette <- palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3","#FF7F00", "#FFFF33", "#A65628"))
 ### Shiny app starts here
+my_palette <- rainbow(7)
 
 # Define UI ----
 ui <- fluidPage( theme = shinytheme("superhero"),
@@ -41,7 +46,7 @@ ui <- fluidPage( theme = shinytheme("superhero"),
                      #h1("Cluster control"),
                      p("Use the controls below to change the appearance of the cluster plot"),
                      sliderInput("clusters", "Number of clusters",
-                                 min = 2, max = 10, value = 5),
+                                 min = 2, max = 7, value = 3),
                      radioButtons(inputId="choice", label="What ellipse type should you use?", 
                                   choices=c("euclid","convex")),
                      p("Understanding the dimensions of the plot"),
@@ -49,7 +54,9 @@ ui <- fluidPage( theme = shinytheme("superhero"),
                    ),
                    mainPanel(
                      h3("Countries in respective clusters"),
-                     plotOutput('plotClusters')
+                     plotOutput('plotClusters'),
+                     h3("World Map country plot"),
+                     plotOutput('plotMap')
                    )
                  )
 )
@@ -58,7 +65,7 @@ server <- function(input, output, session) {
   
   
   clusters <- reactive({
-    kmeans(mydata.scaled, input$clusters, nstart =25)
+    kmeans(mydata.scaled, input$clusters, nstart =50)
   })
   
   ellipse_type <- reactive({
@@ -70,11 +77,9 @@ server <- function(input, output, session) {
   })
   
   output$plotClusters <- renderPlot({
-    palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
-              "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
-    
-    fviz_cluster(clusters(), data = mydata.scaled,
-                 #palette = c("#2E9FDF", "#00AFBB", "#E7B800", "#FC4E07","green","brown"), 
+    fviz_cluster(clusters(), 
+                 data = mydata.scaled,
+                 palette = my_palette,
                  ellipse.type = ellipse_type(), 
                  #labelsize = labels_size(),
                  #pointsize = 0.1,
@@ -86,8 +91,21 @@ server <- function(input, output, session) {
                  ggtheme = theme_minimal()
     )
   },height = function() {
-    session$clientData$output_plotClusters_width*0.71
+    session$clientData$output_plotClusters_width*0.5
   })
+  
+  output$plotMap <- renderPlot({
+
+    cluster = as.numeric(clusters()$cluster)
+    spdf = joinCountryData2Map(data.frame(cluster,df$CountryName), joinCode="NAME", nameJoinColumn="df.CountryName",verbose = TRUE,mapResolution = "low")
+    mapCountryData(spdf, nameColumnToPlot="cluster", catMethod="fixedWidth",colourPalette=my_palette, addLegend = FALSE, lwd = 0.5)
+    
+  },height = function() {
+    session$clientData$output_plotMap_width*0.5
+  })
+  
+  
+  
 }
 
 # Run the app ----
